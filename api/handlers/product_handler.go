dd49ba2e5340c5488773f3649edfa66dddd06c12
NaveenjothiP <36544639+naveenjothi@users.noreply.github.com>
2024-09-22 11:50:19 +0530

    Added Product handlers

---
+++ api/handlers/product_handler.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func CreateProductHandler(ctx *fiber.Ctx, client *mongo.Client) error {
+	return nil
+}
+
+func UpdateProductHandler(ctx *fiber.Ctx, client *mongo.Client) error {
+	return nil
+}
+
+func GetProductHandler(ctx *fiber.Ctx, client *mongo.Client) error {
+	return nil
+}
--- api/routes/routes.go
+++ api/routes/routes.go
@@ -9,4 +9,7 @@ func RegisterFiberRoutes(s *utils.FiberServer) {
 	s.Post("/user", s.WithClient(handlers.CreateUserHandler))
 	s.Get("/user/:id", s.WithClient(handlers.GetUserHandler))
 	s.Post("/user/:id", s.WithClient(handlers.UpdateUserHandler))
+	s.Post("/product", s.WithClient(handlers.CreateProductHandler))
+	s.Get("/product/:id", s.WithClient(handlers.GetProductHandler))
+	s.Post("/product/:id", s.WithClient(handlers.UpdateProductHandler))
 }
