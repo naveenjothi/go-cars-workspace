b4a576839fb5cbda046c6d86339a59786da8b7dc
NaveenjothiP <36544639+naveenjothi@users.noreply.github.com>
2024-10-14 17:04:17 +0530

    updated migration repo

--- migration/repos/migration_repo.go
+++ migration/repos/migration_repo.go
@@ -2,16 +2,17 @@ package repos
 
 import (
 	"libs/base"
+	"migration/models"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MigrationRepo struct {
-	*base.Repository
+	*base.Repository[models.MigrationModel]
 }
 
 func NewMigrationRepo(collection *mongo.Collection) *MigrationRepo {
 	return &MigrationRepo{
-		Repository: base.NewRepository(collection),
+		Repository: base.NewRepository[models.MigrationModel](collection),
 	}
 }
